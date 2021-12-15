1978501d667a4c1f1b70d54d7a1a9e73bc0fc543
kennyho94 <kennyho94@gmail.com>
2021-12-14 19:39:29 -0700

    Initial Commit

---
+++ pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pokedex
+//
+//  Created by Kenny Ho on 12/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
